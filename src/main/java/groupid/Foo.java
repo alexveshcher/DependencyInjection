75fd334996a4430ec5851d8ef6827e1e9a373a11
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2017-02-14 15:41:33 +0200

    Add autowiring using annotations

--- src/main/java/groupid/Foo.java
+++ src/main/java/groupid/Foo.java
@@ -1,15 +1,18 @@
 package groupid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by alex on 2/2/17.
  */
 public class Foo {
+    @Autowired
     private Bar bar;
 
 
-    public void setBar(Bar bar){
-        this.bar = bar;
-    }
+//    public void setBar(Bar bar){
+//        this.bar = bar;
+//    }
 
     public Bar getBar(){
         return bar;
--- src/main/java/groupid/TextEditor.java
+++ src/main/java/groupid/TextEditor.java
@@ -1,8 +1,11 @@
 package groupid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TextEditor {
     private SpellChecker spellChecker;
 
+    @Autowired
     public TextEditor(SpellChecker spellChecker) {
         System.out.println("Inside TextEditor constructor." );
         this.spellChecker = spellChecker;
